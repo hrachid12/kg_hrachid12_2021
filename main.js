25e42b31ca4c481ff40d644e26d7ccd95cba3951
Hassan Rachid <hassan.rachid5@gmail.com>
2021-04-26 17:08:04 -0400

    Added number to phonetic string conversion

--- main.js
+++ main.js
@@ -9,5 +9,20 @@ const num_to_str = {
     8: 'Eight',
     9: 'Nine', 
     0: 'Zero'
+};
+
+for (let i = 2; i < process.argv.length; i++) {
+    let num_as_string = '';
+    let curr_num = parseInt(process.argv[i]);
+    let remainder = 0;
+
+    do {
+        remainder = curr_num % 10;
+        curr_num = Math.floor(curr_num / 10);
+
+        num_as_string = num_to_str[remainder] + num_as_string;
+    } while (curr_num > 0)
+
+    console.log(num_as_string);
 }
 
