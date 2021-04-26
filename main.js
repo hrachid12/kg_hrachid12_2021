a5b122265b5a8bafc75a55688c1341b014e35ed9
Hassan Rachid <hassan.rachid5@gmail.com>
2021-04-26 16:26:25 -0400

    Created a feature branch

---
+++ main.js
@@ -0,0 +1,13 @@
+const num_to_str = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine', 
+    0: 'Zero'
+}
+
